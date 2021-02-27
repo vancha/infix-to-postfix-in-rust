883b78fc28a7f9fd4b2b3262c5d0d04d7946aea4
alittlebitofit <42311431+alittlebitofit@users.noreply.github.com>
2021-02-27 22:45:49 +0530

    changed Token struct from 2 fields to 1 field

--- src/lib.rs
+++ src/lib.rs
@@ -1,91 +1,62 @@
 use std::collections::HashMap;
 use std::fmt;
-/*
- * Consider -
-    Stack S
-    Char  ch
-    Char  element
-while(Tokens are Available)
- {
-     ch = Read(Token);
-     if(ch is Operand)
-       {
-       Print ch ;
-       }
-     else
-       {
-       while(Priority(ch) <= Priority(Top Most Stack))
-            {
-            element = Pop(S);
-            Print(ele);
-            }
-       Push(S,ch);
-       }
-}
-while(!Empty(S))
-{
-element = Pop(S);
-Print(ele);
-}
-*/
 
-#[derive(Debug)] 
+#[derive(Debug)]
 enum TokenType {
-    Number,
-    Operator,
+    Number(i32),
+    Operator(String),
     LeftParenthesis,
     RightParenthesis,
 }
 
 pub struct Token {
     tokentype: TokenType,
-    value: String,
 }
 
 impl Token {
 
-    fn new(tokentype: TokenType, value: String) -> Self {
-        Token { tokentype, value }
+    fn new(token_type: &str) -> Self { 
+        let token = token_type.to_string().parse::<i32>();
+        match token {
+            Ok(value) => Token { tokentype: TokenType::Number(value) },
+            _ => match token_type {
+                "(" => Token { tokentype: TokenType::LeftParenthesis },
+                ")" => Token { tokentype: TokenType::RightParenthesis },
+                "^" => Token { tokentype: TokenType::Operator("^".to_string()) },
+                "*" => Token { tokentype: TokenType::Operator("*".to_string()) },
+                "/" => Token { tokentype: TokenType::Operator("/".to_string()) },
+                "+" => Token { tokentype: TokenType::Operator("+".to_string()) },
+                "-" => Token { tokentype: TokenType::Operator("-".to_string()) },
+                _ => panic!("Don't use invalid values") 
+            }
+        }
     }
 
     fn is_operator(&self) -> bool {
-    	// this comparision doesn't work -.-
-    	// self.tokentype == TokenType::Operator
-    	
         match self.tokentype {
-            TokenType::Operator => true,
-            _ => false,
+            TokenType::Operator(_) => true,
+            _ => false
         }
-        
     }
 
     fn is_number(&self) -> bool {
-    	// this comparision doesn't work -.-
-    	// self.tokentype == TokenType::Number
-
         match self.tokentype {
-            TokenType::Number => true,
-            _ => false,
+            TokenType::Number(_) => true,
+            _ => false
         }
     }
 
     fn is_left_parenthesis(&self) -> bool {
-    	// this comparision doesn't work -.-
-    	// self.tokentype == TokenType::LeftParenthesis
-
         match self.tokentype {
             TokenType::LeftParenthesis => true,
-            _ => false,
+            _ => false
         }
     }
 
     fn is_right_parenthesis(&self) -> bool {
-    	// this comparision doesn't work -.-
-    	// self.tokentype == TokenType::RightParenthesis
-
         match self.tokentype {
             TokenType::RightParenthesis => true,
-            _ => false,
+            _ => false
         }
     }
 
@@ -98,46 +69,57 @@ impl Token {
         precedence.insert("+".to_string(), 2);
         precedence.insert("-".to_string(), 2);
 
-        // return precedence.get(&self.value) >= precedence.get(&other.value);
-        
-        return precedence.get(&self.value) >= precedence.get(&other.value)
-            && &self.value != &other.value;
+        let self_token_type = match &self.tokentype {
+            TokenType::Operator(val) => val.clone(),
+            TokenType::LeftParenthesis => "(".to_string().clone(),
+            TokenType::RightParenthesis => ")".to_string().clone(),
+            _ => {
+                println!("{:?}", self.tokentype);
+                panic!("TokenType should be self operator")
+            }
+        };
+
+        let other_token_type = match &other.tokentype {
+            TokenType::Operator(val) => val,
+            _ => panic!("TokenType should be other operator")
+        };
+
+        return precedence.get::<str>(&self_token_type) >= precedence.get::<str>(&other_token_type)
+            && &self_token_type != other_token_type;
+
     }
+
+    
+    fn operator_value(&self) -> String {
+        match &self.tokentype {
+            TokenType::Operator(val) => val.clone(),
+            _ => panic!("Should be called only on operator")
+    }
+
+}
+
+
 }
 
 impl fmt::Debug for Token {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.value)
+        write!(f, "{:?}", self.tokentype)
     }
 }
-
-// impl std::cmp::PartialEq for TokenType {
-// 	fn eq(&self, otherToken: &TokenType) -> bool {
-// 		self == otherToken
-// 	}
-// }
 
 fn str_to_token(infix: Vec<&str>) -> Vec<Token> {
 
 	let mut token_list: Vec<Token> = vec![];
 
 	for item in infix {
-		let token = item.to_string().parse::<u32>();
-		let token = match token {
-			Ok(_) => Token::new(TokenType::Number, item.to_string()),
-			_ => match &item[..] {
-				"(" => Token::new(TokenType::LeftParenthesis, item.to_string()),
-				")"  => Token::new(TokenType::RightParenthesis, item.to_string()),
-				_ => Token::new(TokenType::Operator, item.to_string())
-			}
-		};
+		let token = Token::new(item);
 		token_list.push(token);
 	}
 
 	token_list
 }
 
-pub fn infix_to_postfix(infix_list: Vec<&str>) -> std::collections::VecDeque<Token> {
+pub fn infix_to_postfix(infix_list: Vec<&str>) -> Vec<String> {// std::collections::VecDeque<Token> {
     let mut outputqueue: std::collections::VecDeque<Token> = std::collections::VecDeque::new();
     let mut operatorstack: Vec<Token> = Vec::new();
     let token_list: Vec<Token> = str_to_token(infix_list);
@@ -154,7 +136,7 @@ pub fn infix_to_postfix(infix_list: Vec<&str>) -> std::collections::VecDeque<Tok
                     .last()
                     .unwrap()
                     .has_greater_precedence_than(&token)
-                && operatorstack.last().unwrap().value != "("
+                && operatorstack.last().unwrap().operator_value() != "(".to_string()
             {
                 outputqueue.push_back(operatorstack.pop().unwrap());
             }
@@ -173,10 +155,28 @@ pub fn infix_to_postfix(infix_list: Vec<&str>) -> std::collections::VecDeque<Tok
         }
 	    
     }
+
     while !operatorstack.is_empty() {
         outputqueue.push_back(operatorstack.pop().unwrap());
     }
-    outputqueue
+
+    // outputqueue
+
+    // Omit this part if return value should be std::collections::VecDeque<Token> instead of Vec<String>    
+    let mut output: Vec<String> = Vec::new();
+    for item in outputqueue {
+        let item = match item.tokentype {
+            TokenType::Operator(val) => val,
+            TokenType::Number(num) => num.to_string(),
+            _ => panic!("Nothing else should be the value")
+        };
+    	output.push(item);
+    }
+    let output = output;
+    output
+    //
+    
+
 }
 
 #[cfg(test)]
@@ -185,6 +185,11 @@ mod tests {
 
     #[test]
     fn test() {
+
+    	assert_eq!(infix_to_postfix(vec!["3", "+", "4", "*", "2",
+	"/", "(", "1", "-", "5", ")",
+	"^", "2", "^", "3"]),
+    	 vec!["3", "4", "2", "*", "1", "5", "-", "2", "3", "^", "^", "/", "+"]);
 
     }
 }
